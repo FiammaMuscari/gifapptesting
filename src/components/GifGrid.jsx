6d52c027399f5323742bf2158bb8f4f35b10cfe1
Fiamma Muscari <fiammamuscari@gmail.com>
2022-07-01 16:09:13 -0300

    first commit

---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,21 @@
+import { useFetchGif } from "../hooks/useFetchGif";
+import { GifItem } from "./GifItem";
+
+
+export const GifGrid = ({category}) => {
+    
+  const {images, loading} = useFetchGif(category);
+
+  return (
+    <>
+        <h3>{category}</h3>
+        <div className="card-grid">
+          {/*Desplegar listado*/}
+          {
+            loading ? <h2>Cargando...</h2> :
+          images.map(imagen => <GifItem key={imagen.id} {...imagen}/> //llamamos el usestate con ...imagen poasamos todas las claves del objeto
+          )}
+        </div>
+      </>
+  )
+}
